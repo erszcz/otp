a099390bc5e6196385fedd778a66f4a26d73c609
Radek Szymczyszyn <lavrin@gmail.com>
2026-02-27 21:17:50 +0100

    Fix file_op/0 type precision in stdlib/src/erl_tar.hrl

--- lib/stdlib/src/erl_tar.hrl
+++ lib/stdlib/src/erl_tar.hrl
@@ -195,11 +195,9 @@
 -type user_data() :: term().
 
 %% Type for the I/O primitive wrapper function
--type file_op() :: fun((write | close | read2 | position,
-                       {user_data(), iodata()} | user_data() | {user_data(), non_neg_integer()}
-                        | {user_data(), non_neg_integer()}) ->
-                              ok | eof | {ok, string() | binary()} | {ok, non_neg_integer()}
-                                 | {error, term()}).
+-type file_op() :: fun((write | close, _) -> ok | {error, term()})
+                 | fun((read2, _) -> {ok, string() | binary()} | eof | {error, term()})
+                 | fun((position, _) -> {ok, integer()} | {error, term()}).
 
 %% These constants (except S_IFMT) are
 %% used to determine what type of device
